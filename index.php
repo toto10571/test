b1ba75d2b8b689919715ae2711b752527b0f5d32
Armel YMG <61087275+toto10571@users.noreply.github.com>
2021-12-10 15:41:14 +0000

    Add files via upload

---
+++ index.php
@@ -0,0 +1,566 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+  <!-- Basic -->
+  <meta charset="utf-8" />
+  <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+  <!-- Mobile Metas -->
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
+  <!-- Site Metas -->
+  <meta name="keywords" content="" />
+  <meta name="description" content="" />
+  <meta name="author" content="" />
+
+  <title>RAVISON TECHNOLOGIE</title>
+
+  <!-- slider stylesheet -->
+  <link rel="stylesheet" type="text/css"
+    href="https://cdnjs.cloudflare.com/ajax/libs/OwlCarousel2/2.1.3/assets/owl.carousel.min.css" />
+
+  <!-- bootstrap core css -->
+  <link rel="stylesheet" type="text/css" href="css/bootstrap.css" />
+
+  <!-- RAVISON Technologie Logo -->
+  <link rel="icon" href="images/logo.png" type="image/gif" />
+
+  <!-- fonts style -->
+  <link href="https://fonts.googleapis.com/css?family=Poppins:400,700|Roboto:400,700&display=swap" rel="stylesheet">
+  <!-- Custom styles for this template -->
+  <link href="css/style.css" rel="stylesheet" />
+  <!-- responsive style -->
+  <link href="css/responsive.css" rel="stylesheet" />
+</head>
+
+<body>
+  <div id="google_translate_element">
+  <div class="hero_area">
+    <!-- header section strats -->
+    <header class="header_section">
+      <div class="container-fluid">
+        <nav class="navbar navbar-expand-lg custom_nav-container ">
+          <a class="navbar-brand" href="index.html">
+            <span>
+              RAVISON Technologie
+            </span>
+          </a>
+          <button class="navbar-toggler ml-auto" type="button" data-toggle="collapse"
+            data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false"
+            aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+          </button>
+
+          <div class="collapse navbar-collapse" id="navbarSupportedContent">
+            <div class="d-flex mx-auto flex-column flex-lg-row align-items-center">
+              <ul class="navbar-nav  ">
+                <li class="nav-item active">
+                  <a class="nav-link" href="index.html">Accueil <span class="sr-only">(current)</span></a>
+                </li>
+                <li class="nav-item">
+                  <a class="nav-link" href="about.html"> A propos de nous </a>
+                </li>
+                <li class="nav-item">
+                  <a class="nav-link" href="service.html"> Nos services </a>
+                </li>
+                <li class="nav-item">
+                  <a class="nav-link" href="contact.html">Nous contacter</a>
+                </li>
+              </ul>
+            </div>
+          </div>
+          <form class="form-inline my-2 my-lg-0 ml-0 ml-lg-4 mb-3 mb-lg-0">
+            <button class="btn  my-2 my-sm-0 nav_search-btn" type="submit"></button>
+          </form>
+        </nav>
+      </div>
+    </header>
+    <!-- end header section -->
+    <!-- slider section -->
+    <section class=" slider_section position-relative">
+      <div id="carouselExampleControls" class="carousel slide" data-ride="carousel">
+        <div class="carousel-inner">
+          <div class="carousel-item active">
+            <div class="slider_item-box layout_padding2">
+              <div class="container">
+                <div class="row">
+                  <div class="col-md-6">
+                    <div class="img-box">
+                      <div>
+                        <img src="images/maintenance.png" alt="" class="" />
+                      </div>
+                    </div>
+                  </div>
+                  <div class="col-md-6">
+                    <div class="detail-box">
+                      <div>
+                        <h1>
+                          <br>
+                          Service de <br>
+                          <span>
+                            Maintenance
+                          </span>
+                        </h1>
+                        <p>
+                          RAVISON Technologie est spécialiste de la maintenance informatique en entreprise pour les TPE et PME. 
+                        </p>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div class="carousel-item ">
+            <div class="slider_item-box layout_padding2">
+              <div class="container">
+                <div class="row">
+                  <div class="col-md-6">
+                    <div class="img-box">
+                      <div>
+                        <img src="images/reseau.png" alt="" class="" />
+                      </div>
+                    </div>
+                  </div>
+                  <div class="col-md-6">
+                    <div class="detail-box">
+                      <div>
+                        <h1>
+                          <br>
+                          Fournitures et<br>
+                          <span>
+                            équipements informatique
+                          </span>
+                        </h1>
+                        <p>
+                          Grâce à ses partenariats avec les plus grandes marques du marché, RAVISON Technologie vous propose un large choix d'équipements informatiques pour votre entreprise.
+                        </p>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div class="carousel-item ">
+            <div class="slider_item-box layout_padding2">
+              <div class="container">
+                <div class="row">
+                  <div class="col-md-6">
+                    <div class="img-box">
+                      <div>
+                        <img src="images/audit.png" alt="" class="" />
+                      </div>
+                    </div>
+                  </div>
+                  <div class="col-md-6">
+                    <div class="detail-box">
+                      <div>
+                        <h1>
+                          <br>
+                          Service <br>
+                          <span>
+                           Audit & Conseil 
+                          </span>
+                        </h1>
+                        <p>
+                          Etudier ou analyser le système en place en vue d’énumérer les problèmes ou limites afin d’apporter des solutions nouvelles tout en tenant compte des besoins du client
+                        </p>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+
+      </div>
+      <div class="container">
+        <div class="slider_nav-box">
+          <div class="btn-box">
+            <a href="service.html">
+              Lire Plus
+            </a>
+            <hr>
+          </div>
+          <div class="custom_carousel-control">
+            <a class="carousel-control-prev" href="#carouselExampleControls" role="button" data-slide="prev">
+              <span class="sr-only">Prev.</span>
+            </a>
+            <a class="carousel-control-next" href="#carouselExampleControls" role="button" data-slide="next">
+              <span class="sr-only">Next</span>
+            </a>
+          </div>
+        </div>
+      </div>
+    </section>
+    <!-- end slider section -->
+  </div>
+  <div class="bg">
+
+    <!-- about section -->
+    <section class="about_section layout_padding">
+      <div class="container">
+        <div class="custom_heading-container">
+          <h3 class=" ">
+            A propos de notre entreprise
+          </h3>
+        </div>
+        <p class="layout_padding2-top">
+          RAVISON Technologie est une Entreprise Burkinabé spécialisée dans l’ingénierie informatique et l’installation de système de contrôle, de surveillance et de sécurité électronique.
+        </p>
+        <div class="img-box layout_padding2">
+          <img src="images/team.png" alt="">
+        </div>
+        <p class="layout_padding2-bottom">
+          Elle apporte son expertise aux sociétés qui ont des difficultés et/ou des besoins en informatique. Son aide pour les petites sociétés qui désirent avoir un service informatique de qualité est inégalable. Intégrateur de solutions informatiques, RAVISON Technologie intervient dans tous les domaines informatiques avec des interventions respectant les normes de qualité. Elle dispose également ... 
+        </p>
+      </div>
+      <div class="container">
+        <div class="btn-box">
+          <a href="about.html">
+            Lire Plus
+          </a>
+          <hr>
+        </div>
+      </div>
+    </section>
+
+
+    <!-- end about section -->
+
+    <!-- service section -->
+
+    <section class="service_section layout_padding-bottom">
+      <div class="container">
+        <div class="custom_heading-container">
+          <h3 class=" ">
+            NOS SERVICES
+          </h3>
+        </div>
+        <p class="">
+          Découvrez nos services!
+        </p>
+        <div class="service_container ">
+          <div class="row">
+            <div class="col-md-3">
+              <div class="box b-1">
+                <div class="img-box">
+                  <img src="images/maintenance.png" alt="">
+                </div>
+                <div class="detail-box">
+                  <h6>
+                    Maintenance
+                  </h6>
+                  <p>
+                    Maintenance des équipements informatiques
+                  </p>
+                  <div class="btn-box">
+                    <a href="maintenance.html">
+                      Lire Plus
+                    </a>
+                    <hr>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div class="col-md-3">
+              <div class="box b-2">
+                <div class="img-box">
+                  <img src="images/securite.png" alt="">
+                </div>
+                <div class="detail-box">
+                  <h6>
+                    Sécurité
+                  </h6>
+                  <p>
+                    incendie • vidéo surveillance • Contrôle d’accès 
+                  </p>
+                  <div class="btn-box">
+                    <a href="securite.html">
+                      Lire Plus
+                    </a>
+                    <hr>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div class="col-md-3">
+              <div class="box b-3">
+                <div class="img-box">
+                  <img src="images/audit.png" alt="">
+                </div>
+                <div class="detail-box">
+                  <h6>
+                    Audit & Conseil
+                  </h6>
+                  <p>
+                    Audit & Conseil
+                  </p>
+                  <div class="btn-box">
+                    <a href="">
+                      Lire Plus
+                    </a>
+                    <hr>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div class="col-md-3">
+              <div class="box b-4">
+                <div class="img-box">
+                  <img src="images/reseau.png" alt="">
+                </div>
+                <div class="detail-box">
+                  <h6>
+                    Réseaux informatiques
+                  </h6>
+                  <p>
+                    Gestion complète de votre réseau informatique
+                  </p>
+                  <div class="btn-box">
+                    <a href="">
+                      Lire Plus
+                    </a>
+                    <hr>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+
+    <!-- end service section -->
+
+    <!-- work section -->
+
+    <section class="work_section layout_padding">
+      <div class="container">
+        <div class="custom_heading-container">
+          <h3 class=" ">
+            Comment fonctionne t-on ?
+          </h3>
+        </div>
+      </div>
+      <div class="work_container">
+        <div class="box b-1">
+          <div class="img-box">
+            <img src="images/etude.png" alt="">
+          </div>
+          <div class="name">
+            <h6>
+              Etude de votre projet
+            </h6>
+          </div>
+        </div>
+        <div class="box b-2">
+          <div class="img-box ">
+            <img src="images/solutions.png" alt="">
+          </div>
+          <div class="name">
+            <h6>
+              Proposition de solutions adéquates
+            </h6>
+          </div>
+        </div>
+        <div class="box b-3">
+          <div class="img-box ">
+            <img src="images/mise.png" alt="">
+          </div>
+          <div class="name">
+            <h6>
+              Mise en place
+            </h6>
+          </div>
+        </div>
+        <div class="box b-4">
+          <div class="img-box ">
+            <img src="images/formation.png" alt="">
+          </div>
+          <div class="name">
+            <h6>
+              Formation 
+            </h6>
+          </div>
+        </div>
+        <div class="box b-5">
+          <div class="img-box">
+            <img src="images/maintenance.png" alt="">
+          </div>
+            <h6>
+              Maintenance & </br>Assistance
+            </h6>
+        </div>
+      </div>
+    </section>
+
+
+    <!-- end work section -->
+
+    <!-- contact section -->
+
+    <section class="contact_section layout_padding">
+      <div class="custom_heading-container">
+        <h3 class=" ">
+          Obtenir un devis
+        </h3>
+      </div>
+      <div class="container layout_padding2-top">
+        <div class="row">
+          <div class="col-md-6 mx-auto">
+            <form name="contact" method="POST" data-netlify="true">
+              <div>
+                <input type="text" name="nom" placeholder="NOM">
+              </div>
+              <div>
+                <input type="email" name="email" placeholder="EMAIL">
+              </div>
+              <div>
+                <input type="text" name="tel" placeholder="TELEPHONE">
+              </div>
+              <div>
+                <select name="service[]">
+                  <option value="" disabled selected>TYPE DE SERVICE</option>
+                  <option value="Maintenance Informatique">Maintenance Informatique</option>
+                  <option value="Sécurité">Sécurité</option>
+                  <option value="Audit & Conseil">Audit & Conseil</option>
+                  <option value="Fourniture & équipements informatiques">Fourniture & équipements informatiques</option>
+                </select>
+              </div>
+              <div>
+                <input type="text" name="message" class="message-box" placeholder="Votre message">
+              </div>
+              <div class="d-flex justify-content-center ">
+                <button>
+                  ENVOYER
+                </button>
+              </div>
+            </form>
+          </div>
+        </div>
+
+      </div>
+    </section>
+
+    <!-- end contact section -->
+
+    <!-- info section -->
+    <section class="info_section layout_padding">
+      <div class="container">
+        <div class="row">
+          <div class="col-md-3">
+            <div class="info-logo">
+              <h2>
+                RAVISON Technologie
+              </h2>
+              <p>
+                RAVISON Technologie est une Entreprise Burkinabé spécialisée dans la prestation de services informatiques et Technologiques. 
+              </p>
+            </div>
+          </div>
+          <div class="col-md-3">
+            <div class="info-nav">
+              <h4>
+                Navigation
+              </h4>
+              <ul>
+                <li>
+                  <a href="index.html">
+                    Accueil
+                  </a>
+                </li>
+                <li>
+                  <a href="about.html">
+                    A propos de nous
+                  </a>
+                </li>
+                <li>
+                  <a href="service.html">
+                    Nos services
+                  </a>
+                </li>
+                <li>
+                  <a href="contact.html">
+                    Nous contacter
+                  </a>
+                </li>
+              </ul>
+            </div>
+          </div>
+          <div class="col-md-3">
+            <div class="discover">
+              <h4>
+                NEWSLETTER
+              </h4>
+              <ul>
+                <li>
+                  <form name="souscription newsletter" method="POST" data-netlify="true">
+                    <input name="email" type="text" placeholder="Entrez votre email" />
+                    <button type="submit">
+                      Souscrire
+                    </button>
+                  </form>
+                </li>
+              </ul>
+              <div class="social-box">
+                <a href="www.facebook.com/Ravison_Technologie-100698585024588">
+                  <img src="images/facebook.png" alt="">
+                </a>
+              </div>
+            </div>
+          </div>
+          <div class="col-md-3">
+            <div class="info-contact">
+              <h4>
+                Adresse
+              </h4>
+              <div class="location">
+                <a href="">
+                  <img src="images/location.png" alt="">
+                  <span>
+                    Ouagadougou, Burkina Faso
+                  </span>
+                </a>
+              </div>
+              <div class="call">
+                <a href="">
+                  <img src="images/telephone.png" alt="">
+                  <span>
+                    ( +226 ) 70 06 39 30
+                  </span>
+                </a>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+
+
+
+    <!-- end info_section -->
+
+    <!-- footer section -->
+    <section class="container-fluid footer_section">
+      <p>
+        Copyright &copy; 2021 All Rights Reserved By
+        <a href="https://perfectdesign226.com/">Perfect Design 226</a>
+      </p>
+    </section>
+    <!-- footer section -->
+
+    <script type="text/javascript" src="js/jquery-3.4.1.min.js"></script>
+    <script type="text/javascript" src="js/bootstrap.js"></script>
+    <script type="text/javascript" src="//translate.google.com/translate_a/element.js?cb=googleTranslateElementInit"></script>
+    <script type="text/javascript">
+      function googleTranslateElementInit(){
+        new google.translate.TranslateElement({pageLanguage : 'fr'}, 'google_translate_element'); 
+      }
+    </script>
+  </div>
+</body>
+
+
+</html>
